500464a1106378d11f32b3f6c31b5d45266e93ad
Alexandru Ifrim <ifrim.alexandru0@gmail.com>
2022-01-27 12:13:49 +0200

    First Automated Tests

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        private const string homeUrl = "http://localhost:44108/";
+        private const string aboutUrl = "http://localhost:44108/Home/About";
+        private const string homeTitle = "Home Page - Credit Cards";
+        private const string aboutTitle = "About - Credit Cards";
+
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+              
+
+                driver.Navigate().GoToUrl(homeUrl);
+
+                DemoHelper.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+
+        public void ReloadHomePage() 
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            { 
+               
+                driver.Navigate().GoToUrl(homeUrl);
+
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+
+            }
+
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+
+        public void ReloadHomePageOnBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+                IWebElement generationTokenElement = driver.FindElement(By.Id("GenerationToken"));
+                string initialToken = generationTokenElement.Text;
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(aboutUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+
+                string reloadToken = driver.FindElement(By.Id("GenerationToken")).Text;
+                Assert.NotEqual(initialToken, reloadToken);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+
+        public void ReloadHomePageOnForwardV2()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(aboutUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+                driver.Navigate().Forward();
+                Assert.Equal(aboutTitle, driver.Title);
+                Assert.Equal(aboutUrl, driver.Url);
+
+
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+
+        public void ReloadHomePageonForward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(aboutUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().GoToUrl(homeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                driver.Navigate().Forward();
+                DemoHelper.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+
+                //ToDo: assert that page was reloaded
+            }
+        }
+    }
+}
---
+++ CreditCards.UITests/DemoHelper.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Threading;
+
+
+namespace CreditCards.UITests
+{
+     internal static class DemoHelper
+    {
+        ////<summary>
+        ///Brief delay to slow down browser interaction for
+        ///demo video recording purposes
+        ///</summay>
+
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+
+    }
+}
